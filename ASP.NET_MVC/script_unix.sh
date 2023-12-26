b7e6e96c7a06e1aeddfbe77937915804821247b6
Dtammie <dtammie@student.21-school.ru>
2023-12-26 22:12:15 +0700

    UPD scripts for deploy in all versions

--- ASP.NET_MVC/script_unix.sh
+++ ASP.NET_MVC/script_unix.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 $current_preset="default"
-$lib_dir_for_copy="..\ASP.NET_MVC\Libraries"
+$lib_dir_for_copy="../ASP.NET_MVC/Libraries"
 $lib_name="ModelLibrary.dll"
 
-cd ..\CalculatorLibraryCPP &&
-cmake --preset=$current_preset &&
-cmake --build --preset=$current_preset &&
-mkdir -p $lib_dir_for_copy &&
-Copy-Item -Path ".\out\$lib_name" -Destination "$lib_dir_for_copy\$lib_name" &&
-rm out -R &&
-cd ..\ASP.NET_MVC &&
+cd ../CalculatorLibraryCPP
+cmake --preset=$current_preset
+cmake --build --preset=$current_preset 
+mkdir -p $lib_dir_for_copy 
+cp out/$lib_name $lib_dir_for_copy/$lib_name
+rm -rm out
+cd ../ASP.NET_MVC
 dotnet run
--- MAUI/script_unix.sh
+++ MAUI/script_unix.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+
 $current_preset="default"
-$lib_dir_for_copy="..\MAUI\MyCalculator_v3.0\Libraries"
+$lib_dir_for_copy="../MAUI/MyCalculator_v3.0/Libraries"
 $lib_name="ModelLibrary.dll"
 $target_framework = "net8.0-maccatalyst"
 $configuration = "maccatalyst-x64"
-$open_path = ".\MyCalculator_v3.0\bin\Debug\$target_framework\$configuration\publish\MyCalculator"
+$open_path = "./MyCalculator_v3.0/bin/Debug/$target_framework/$configuration/publish/MyCalculator"
 
-cd ..\CalculatorLibraryCPP &&
-cmake --preset=$current_preset &&
-cmake --build --preset=$current_preset &&
-mkdir -p $lib_dir_for_copy &&
-Copy-Item -Path ".\out\$lib_name" -Destination "$lib_dir_for_copy\$lib_name" &&
-rm out -R &&
-cd ..\MAUI &&
-dotnet publish .\MyCalculator_v3.0 -f $target_framework -c Debug -p:RuntimeIdentifier=$configuration &&
+cd ../CalculatorLibraryCPP
+cmake --preset=$current_preset
+cmake --build --preset=$current_preset
+mkdir -p $lib_dir_for_copy
+cp out/$lib_name $lib_dir_for_copy/$lib_name
+rm -rf out -R
+cd ../MAUI
+dotnet publish ./MyCalculator_v3.0 -f $target_framework -c Debug -p:RuntimeIdentifier=$configuration
 open $open_path
